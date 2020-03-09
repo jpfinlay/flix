662c73b3b5108778b4333fe32e50519704f8748e
Jamie Finlay <jpfinlay@gmail.com>
2020-03-09 23:54:39 +0000

    Create migration for description and released_on database fields.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_02_224906) do
+ActiveRecord::Schema.define(version: 2020_03_09_233919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2020_03_02_224906) do
     t.decimal "total_gross"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
+    t.datetime "released_on"
   end
 
 end
