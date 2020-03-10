aeefb18f78dbaead765738328cc99621aff95051
Jamie Finlay <jpfinlay@gmail.com>
2020-03-10 17:42:19 +0000

    Create seeds.rb file and tidy up index views with helper methods.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,45 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+movies = Movie.create([
+  { 
+    title:        "Star Wars: Episode IV - A New Hope", 
+    rating:       "U",
+    total_gross:  775512064,
+    description:  "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee and two droids to save the galaxy from the Empire's world-destroying battle station, while also attempting to rescue Princess Leia from the mysterious Darth Vader.",
+    released_on:  "1978-01-29"
+  },
+  {
+    title:        "Star Wars: Episode V - The Empire Strikes Back", 
+    rating:       "U",
+    total_gross:  547897454,
+    description:  "After the Rebels are brutally overpowered by the Empire on the ice planet Hoth, Luke Skywalker begins Jedi training with Yoda, while his friends are pursued by Darth Vader and a bounty hunter named Boba Fett all over the galaxy.", 
+    released_on:  "1980-05-21"
+  },
+  {
+    title:        "Superman",
+    rating:       "A",
+    total_gross:  300451603,
+    description:  "An alien orphan is sent from his dying planet to Earth, where he grows up to become his adoptive home's first and greatest superhero.",
+    released_on:  "1978-12-14"
+  },
+  {
+    title:        "Iron Man",
+    rating:       "12A",
+    total_gross:  585366247,
+    description:  "After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.",
+    released_on:  "2008-05-02"
+  },
+  {
+    title:        "Spider-Man",
+    rating:       "12A",
+    total_gross:  825025036,
+    description:  "When bitten by a genetically modified spider, a nerdy, shy, and awkward high school student gains spider-like abilities that he eventually must use to fight evil as a superhero after tragedy befalls his family.",
+    released_on:  "2002-06-14"
+  },
+  {
+    title:        "Thor",
+    rating:       "12A",
+    total_gross:  449326618,
+    description:  "The powerful but arrogant god Thor is cast out of Asgard to live amongst humans in Midgard (Earth), where he soon becomes one of their finest defenders.",
+    released_on:  "2011-04-27"
+  }
+])
